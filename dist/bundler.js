b299828eca7f186188c6ecc60ce49c39b4ee5b96
raingrain <2711835261@qq.com>
2024-01-24 20:25:18 +0800

    change color

--- dist/bundler.js
+++ dist/bundler.js
@@ -60,7 +60,7 @@ function addChatBotMessage(message) {
 
 function askQuestion() {
     // 读取用户输入
-    const userInput = readlineSync.question(colors.rainbow("You: "));
+    const userInput = readlineSync.question(colors.yellow("You: "));
     // 将用户消息加入上下文环境
     addUserMessage(userInput);
     // 并返回
